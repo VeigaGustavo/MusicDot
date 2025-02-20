0cfb8b21acd374833ec67281b22fbcb365d434eb
Gustavo Antunes Veiga do Carmo <gustavo.carmo@estudante.ifto.edu.br>
2025-02-20 09:36:17 -0300

    Topico CSS | Trabalho 1

---
+++ script.js
@@ -0,0 +1,11 @@
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const formObject = Object.fromEntries(formData.entries());
+
+    console.log('Form Data:', formObject);
+    alert('Formulário enviado com sucesso!');
+
+    event.target.reset();
+}
